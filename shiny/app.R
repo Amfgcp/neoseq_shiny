c451a38b0caf52b75b1bae36bf637f523025e5af
pathreg <parthreglumc@gmail.com>
2019-02-07 16:00:24 +0100

    added a folder select button, and print the folder content to the main panel

--- shiny/app.R
+++ shiny/app.R
@@ -1,36 +1,70 @@
 library(shiny)
+#library(shinyFiles) #this library is used, but does not need to be loaded since we only call it via shinyFiles::[function]
 
-# Define UI for application that plots random distributions 
 ui = fluidPage(
-  
-  # Application title
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar with a slider input for number of observations
+  #titlePanel("Hello Shiny!"),
+
   sidebarLayout(
     sidebarPanel(
-      sliderInput("obs", 
-                  "Number of observations:", 
-                  min = 1, 
-                  max = 1000, 
-                  value = 500)
+      #folder input. Creates the input button with some informative texts. 
+      p(style = "font-size: 96%; font-weight: bold;", "Select folder:"),
+      shinyFiles::shinyDirButton("dir", "Browse...", "Select folder"), #this function is coupled to shinyDirChoose in the server section.
+      textOutput("selected.folder", inline = T) #states the currently selected folder
     ),
     
     mainPanel(
-      plotOutput("distPlot")
+      textOutput("folder")
     )
   )
 )
 
 server = function(input, output, session) {
+  #########################################################
+  # folder input
+  #########################################################
   
-  output$distPlot <- renderPlot({
-    # generate an rnorm distribution and plot it
-    dist <- rnorm(input$obs)
-    hist(dist)
+  #1) create a named list of places where users can start searching
+  roots = c(unique(substr(list.dirs(paste0(LETTERS, ":/"), recursive = F), 1, 3)), getwd(), "O:/Transfer/Siebren/17-CHPv4c_week5run3-NORMALStest2")
+  names(roots) <- c(roots[1:(length(roots)-2)], getwd(), "demo_location")
+  
+  #2) use the named list to allow users to select a folder. This function is coupled to shinyDirButton in the ui section.
+  shinyFiles::shinyDirChoose(input, 'dir',  updateFreq = 0, roots = roots, defaultRoot = "demo_location")
+  
+  #3) harvest data from the user's selected folder
+  folder.info <- reactiveValues() #all data will be stored in this reactiveValues object.
+  observeEvent(input$dir, #priority = 2,
+    #input$dir only returns a second variable if a folder is selected
+    if(!is.na(input$dir[2])){
+      folder.info$path <- shinyFiles::parseDirPath(roots, input$dir) #this location needs to be specified alot of times, so we save it here
+     
+      #list all content
+      folder.info$content = list.files(folder.info$path)#, pattern = ".vcf$")
+      #folder.info$n = length(folder.info$content)
+    }
+  )
+  
+  
+  
+  #########################################################
+  # folder output
+  #########################################################
+  
+  #states the currently selected folder
+  output$selected.folder <- renderPrint(
+    #input$dir only returns a second variable if a folder is selected
+    if(!is.na(input$dir[2]))cat("Selected folder: ", folder.info$path) else cat("No folder selected")
+  )
+  
+  #prints the names of all files and folders in the selected folder
+  output$folder <- renderText({
+    folder.info$content
   })
   
   
+  
+  #########################################################
+  # other
+  #########################################################
   
   #terminate this app when the browser window is closed.
   session$onSessionEnded(function() {
